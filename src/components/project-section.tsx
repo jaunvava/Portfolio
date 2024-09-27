a54fa9c61a5a02003a9efa4ac921b083e69278f9
jaunvava <mortalteck1020@gmail.com>
2024-09-27 02:41:13 -0300

    alt: alguns ajustes

--- src/components/project-section.tsx
+++ src/components/project-section.tsx
@@ -92,7 +92,7 @@ export default function ProjectSectionDeploy() {
   const [selectedProject, setSelectedProject] = useState<Project | null>(null);
 
   return (
-    <section className="py-12  bg-gray-50 dark:bg-secondary">
+    <section className="py-16  bg-gray-50 dark:bg-secondary">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-8 ">Meus Projetos</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
